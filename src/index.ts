a1a77892122af42cea535dab44f26d878bcc40cc
Justin Yum <justinyum98@gmail.com>
2023-03-08 20:54:22 -0800

    configured eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   // TODO: Add `winston` logging framework, replace below
   // tslint:disable-next-line:no-console
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
